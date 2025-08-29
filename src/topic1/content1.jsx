d34fef37df0add3063a027ef76bc2c36b92fdc3c
unknown <aaron.fian.seabs@ac.id>
2025-08-29 15:36:45 +0700

    fix content so the design is responsive (following safe area) and minimize content header size and fontsize

--- src/topic1/content1.jsx
+++ src/topic1/content1.jsx
@@ -20,7 +20,7 @@ export default function Topic1Content1() {
     <div className="topic1-container">
       <div className="topic1-header">
         <div className="topic1-title-group">
-          <Link to="/choosetopic" className="arrow-btn-top-left">
+          <Link to="/choosetopic" className="topic1-arrow-btn-top-left">
             <MdChevronLeft />
           </Link>
           <div className="topic1-title">{topic1texts[lang].titlecontent}</div>
